907a8ffa3f7402cf681ed2abb48e5ded63246f3a
warleon <51892390+warleon@users.noreply.github.com>
2025-08-15 16:55:09 -0500

    Update useBackendSubscription.ts

--- frontend/speech2text/hooks/useBackendSubscription.ts
+++ frontend/speech2text/hooks/useBackendSubscription.ts
@@ -1,14 +1,54 @@
-import { useMemo } from "react";
+import {
+  backendResponse,
+  languageDetectionResponse,
+  mergeResponse,
+  toNumpyResponse,
+  transcriptionResponse,
+  voiceSegmentsDetectionResponse,
+} from "@/types/backend";
+import { useEffect, useMemo } from "react";
 import useWebSocket from "react-use-websocket";
 
 interface Props {
   user: string;
+  on?: {
+    transcription?: (r: transcriptionResponse) => void | Promise<void>;
+    languageFound?: (r: languageDetectionResponse) => void | Promise<void>;
+    segmentation?: (r: voiceSegmentsDetectionResponse) => void | Promise<void>;
+    merge?: (r: mergeResponse) => void | Promise<void>;
+    preProcess?: (r: toNumpyResponse) => void | Promise<void>;
+  };
 }
 
-export function useBackendSubscription({ user }: Props) {
+export function useBackendSubscription({ user, on }: Props) {
   const connectionString = useMemo(() => {
     const host = window.location.host;
     return `ws://${host}/api/ws?user=${user}`; //TODO: should be url sanitized
   }, [user]);
-  const { lastJsonMessage } = useWebSocket(connectionString);
+  const { lastJsonMessage } = useWebSocket<backendResponse>(connectionString);
+  useEffect(() => {
+    switch (lastJsonMessage.task_type) {
+      case "convert_to_numpy":
+        if (on?.preProcess) on.preProcess(lastJsonMessage as toNumpyResponse);
+        break;
+      case "detect_language":
+        if (on?.languageFound)
+          on.languageFound(lastJsonMessage as languageDetectionResponse);
+        break;
+      case "detect_voice_segments":
+        if (on?.segmentation)
+          on.segmentation(lastJsonMessage as voiceSegmentsDetectionResponse);
+        break;
+      case "merge_jobs":
+        if (on?.merge) on.merge(lastJsonMessage as mergeResponse);
+        break;
+      case "transcribe_segment":
+        if (on?.transcription)
+          on.transcription(lastJsonMessage as transcriptionResponse);
+        break;
+
+      default:
+        break;
+    }
+  }, [lastJsonMessage]);
 }
